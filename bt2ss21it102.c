d8e79fd8843a180c7ab8c937e2419a9f65433e4d
langhongphuc <langphuc27@gmail.com>
2025-11-19 21:00:35 +0700

    LangHongPhuc_B25DTCN199_IT102_SS21_BTVN2

---
+++ bt2ss21it102.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+struct Student{
+	char name[50];
+	int age;
+	float grade; 
+};
+
+int main(){
+	struct Student student1;
+	printf("Nhap ten hoc sinh: ");
+	fgets(student1.name, sizeof(student1.name), stdin);
+	
+	printf("Nhap tuoi cua hoc sinh: ");
+	scanf("%d", &student1.age);
+	
+	printf("Nhap tuoi cua hoc sinh: ");
+	scanf("%d", &student1.grade);
+	
+	printf("----------Thong tin hoc sinh----------\n");
+	printf("Ten:%s", student1.name);
+	printf("Tuoi:%d\n", student1.age);
+	printf("Diem trung binh:%.2f\n", student1.grade);
+	
+	return 0; 
+} 
+ 
